9177ddf663da26ff9c317b36cf43cc9f5475dce8
Himanshu Khandelwal <30552938+himanshu-100@users.noreply.github.com>
2019-03-20 20:37:30 +0530

    No need to change color everytime now

--- background.js
+++ background.js
@@ -23,8 +23,8 @@ var rule2={
 };
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+  chrome.storage.sync.set({color: '#000000'}, function() {
+    console.log("The color is white.");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([rule2]);
--- popup.js
+++ popup.js
@@ -1,14 +1,27 @@
 let changeColor = document.getElementById('changeColor');
-
   chrome.storage.sync.get('color', function(data) {
   	var e=changeColor.style.backgroundColor;
     changeColor.setAttribute('value', e);
   });
   changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+  	let color = element.target.value;
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    chrome.tabs.executeScript(
+	        tabs[0].id,
+	        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+	});
+	chrome.storage.sync.set({color: color}, function() {
+    	console.log("The color is "+color+".");
+  	});
+  };
+  changeColor.onload = function(element) {
+
+  	chrome.storage.sync.get('color', function(data) {
+  	var color=data.color;
+  	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    chrome.tabs.executeScript(
+	        tabs[0].id,
+	        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+	});
+  	});
   };
